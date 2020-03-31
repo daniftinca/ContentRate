75f904ae77692b0365fcbe8300a97a0c84b5dd47
Daiana Tifenbacher <dtifenbacher@gmail.com>
2020-03-31 21:42:45 +0300

    - login & logout

--- content_rate/urls.py
+++ content_rate/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('scrape/', include('content_scrape.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
