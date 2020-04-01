b8f758cc6815d5cd46d766fd5ab900d008aa47da
Dan <daniftincaalexandru@gmail.com>
2020-04-01 11:57:49 +0300

    added tfidf

--- content_analyzer/service/analyzer_service.py
+++ content_analyzer/service/analyzer_service.py
@@ -1,6 +1,9 @@
 from content_scrape.service.content_scraper_service import ContentScraperService
 from content_scrape.service.google_search_scraper_service import GoogleSearchScraperService
-
+import pandas as pd
+import sklearn as sk
+import math
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class AnalyzerService:
     url = ''
@@ -25,3 +28,75 @@ class AnalyzerService:
         request_length = len(request_page_content.split(" "))
 
         return request_length,target_length
+
+
+    def compare_tfidf_new(self):
+        tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+
+        first = self.scraping_service.get_page_content(self.url)
+        others = self.google_scrape_service.get_search_result_with_content(self.target_query)
+        results = []
+        for content in others:
+            tfidf_vectorizer_vectors = tfidf_vectorizer.fit_transform([first,content])
+            first_vector_tfidfvectorizer = tfidf_vectorizer_vectors[0]
+            df = pd.DataFrame(first_vector_tfidfvectorizer.T.todense(), index=tfidf_vectorizer.get_feature_names(),
+                              columns=["tfidf"])
+            df.sort_values(by=["tfidf"], ascending=False)
+            results.append(df)
+
+        return results
+
+    def compare_tfidf(self):
+        first = self.scraping_service.get_page_content(self.url)
+        others = self.google_scrape_service.get_search_result_with_content(self.target_query)
+        results = []
+        for content in others:
+            total = set(first).union(set(content))
+            wordDictA = dict.fromkeys(total, 0)
+            wordDictB = dict.fromkeys(total, 0)
+            for word in first:
+                wordDictA[word] += 1
+
+            for word in content:
+                wordDictB[word] += 1
+
+            pd.DataFrame([wordDictA, wordDictB])
+            tfFirst = self.computeTF(wordDictA, first)
+            tfSecond = self.computeTF(wordDictB, content)
+
+            idfs = self.computeIDF([wordDictA, wordDictB])
+
+            idfFirst = self.computeTFIDF(tfFirst, idfs)
+            idfSecond = self.computeTFIDF(tfSecond, idfs)
+
+            tfidf = pd.DataFrame([idfFirst, idfSecond])
+
+            results.append(tfidf)
+
+    def computeTF(self, wordDict, bow):
+        tfDict = {}
+        bowCount = len(bow)
+        for word, count in wordDict.items():
+            tfDict[word] = count / float(bowCount)
+        return tfDict
+
+    def computeIDF(self, docList):
+        idfDict = {}
+        N = len(docList)
+
+        idfDict = dict.fromkeys(docList[0].keys(), 0)
+        for doc in docList:
+            for word, val in doc.items():
+                if val > 0:
+                    idfDict[word] += 1
+
+        for word, val in idfDict.items():
+            idfDict[word] = math.log10(N / float(val))
+
+        return idfDict
+
+    def computeTFIDF(self, tfBow, idfs):
+        tfidf = {}
+        for word, val in tfBow.items():
+            tfidf[word] = val * idfs[word]
+        return tfidf
